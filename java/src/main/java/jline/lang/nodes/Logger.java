bebe8ff40e3fd9b6b89fc9b963314d937aabd778
Giuliano Casale <g.casale@imperial.ac.uk>
2023-12-05 11:18:42 +0000

    Regenerated javadoc

--- java/src/main/java/jline/lang/nodes/Logger.java
+++ java/src/main/java/jline/lang/nodes/Logger.java
@@ -11,10 +11,15 @@ import jline.lang.sections.Forker;
 public class Logger extends Node implements Serializable {
 
     protected Network model;
-    public Logger(Network model) {
+    protected String fileName;
+
+    public Logger(Network model,  String name, String logfile) {
+        // TODO: this class doesn't work.
         super("Logger");
+        //this.setName(name);
         model.addNode(this);
         this.model = model;
+        //this.fileName = logfile;
     }
 
     @Override
