9a2fffdaf9f857693e111fbe03ffdbea2d4b0c99
Giuliano Casale <g.casale@imperial.ac.uk>
2023-12-30 10:52:49 +0000

    m feat: improved propfair

--- matlab/src/api/pfqn/pfqn_propfair.m
+++ matlab/src/api/pfqn/pfqn_propfair.m
@@ -18,10 +18,10 @@ function [G,lG,Xasy] = pfqn_propfair(L,N,Z)
 
 [M,R]=size(L);
 optimopt = optimoptions(@fmincon,'MaxFunctionEvaluations',1e6,'Display','none');
-obj = @(X) -sum(N.*log(X+1e-6));
-x0 = N ./ (N.*sum(L,1)+Z);
+obj = @(X) -sum((N-X.*Z).*log(X+1e-6));
+x0 = N ./ (sum(L,1) + (sum(N)-1)*max(L,[],1) + Z);
 [Xopt] = fmincon(@(x) obj(x), x0, L, ones(M,1), [],[], zeros(1,R), [],[],optimopt);
-Xasy = Xopt .* N ./ (N + Z.*Xopt); % add think time. If embedded in the optimization program it goes worse.
+Xasy = Xopt .* (sum(L,1) + (sum(N)-1)*max(L,[],1)) ./ (sum(L,1) + (sum(N)-1)*max(L,[],1) + Z); 
 lG = -sum(N.*log(Xasy));
 G = exp(lG);
 end
