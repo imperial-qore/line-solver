ae560bfe9bd58468d581e2bb425c3d272fd3181d
Giuliano Casale <g.casale@imperial.ac.uk>
2024-01-13 19:09:13 +0000

    p: added initial unit test for pfqn API

--- python/line_solver/api.py
+++ python/line_solver/api.py
@@ -15,8 +15,10 @@ def cache_mva(gamma, m):
     E = jlineMatrixFromArray(ret.E)
     return pi, pi0, pij, x, u, E
 
+
 def cache_prob_asy(gamma, m):
     return jpype.JPackage('jline').api.CACHE.cache_prob_asy(jlineMatrixFromArray(gamma), jlineMatrixFromArray(m))
+
 
 def ctmc_uniformization(pi0, Q, t):
     return jlineMatrixToArray(
@@ -38,11 +40,29 @@ def ctmc_solve(matrix):
 def dtmc_solve(matrix):
     return jlineMatrixToArray(jpype.JPackage('jline').api.DTMC.dtmc_solve(jlineMatrixFromArray(matrix)))
 
+
 def dtmc_stochcomp(matrix, indexes):
     ind = jpype.java.util.ArrayList()
     for i in range(len(indexes)):
         ind.add(JInt(indexes[i]))
-    return jlineMatrixToArray(jpype.JPackage('jline').api.DTMC.dtmc_stochcomp(jlineMatrixFromArray(matrix),ind))
+    return jlineMatrixToArray(jpype.JPackage('jline').api.DTMC.dtmc_stochcomp(jlineMatrixFromArray(matrix), ind))
+
 
 def dtmc_timereverse(matrix):
     return jlineMatrixToArray(jpype.JPackage('jline').api.DTMC.dtmc_timereverse(jlineMatrixFromArray(matrix)))
+
+
+def pfqn_ca(L, N, Z):
+    pfqnNcReturn = jpype.JPackage('jline').api.PFQN.pfqn_ca(jlineMatrixFromArray(L), jlineMatrixFromArray(N),
+                                                            jlineMatrixFromArray(Z))
+    return pfqnNcReturn.G, pfqnNcReturn.lG
+
+
+def pfqn_bs(L, N, Z):
+    pfqnBSReturn = jpype.JPackage('jline').api.PFQN.pfqn_bs(jlineMatrixFromArray(L), jlineMatrixFromArray(N),
+                                                            jlineMatrixFromArray(Z))
+    XN = jlineMatrixToArray(pfqnBSReturn.XN)
+    QN = jlineMatrixToArray(pfqnBSReturn.QN)
+    UN = jlineMatrixToArray(pfqnBSReturn.UN)
+    RN = jlineMatrixToArray(pfqnBSReturn.RN)
+    return XN[0], QN, UN, RN, pfqnBSReturn.it
