4ad42deb0ba6535f36109d9833376423d626e069
Giuliano Casale <g.casale@imperial.ac.uk>
2026-07-24 19:10:06 +0200

    Minor release 3.0.6 (from dev @ 39274be2c)

--- matlab/examples/inference/est_rnn_closed.m
+++ matlab/examples/inference/est_rnn_closed.m
@@ -55,3 +55,29 @@ solver{1} = MVA(model);
 fprintf(1, '\nSOLVER: %s\n', solver{1}.getName());
 AvgTable{1} = solver{1}.getAvgTable();
 AvgTable{1}
+
+%% Local helper: SSA-based queue-length trace generation
+function [timeIntervals, queueLengthMatrix] = infer_generate_qlen_traces(model, stride)
+% Use the SSA solver to generate subsampled queue-length traces
+    sn = model.getStruct();
+    nStateful = sn.nstateful;
+    jobCount = sn.nclasses;
+
+    solver = SolverSSA(model, 'force', true, 'timespan', [0, Inf], ...
+        'samples', 10000, 'method', 'serial');
+    ts = solver.sampleSysAggr(10000);
+
+    % Subsample by stride
+    nSamples = length(ts.t);
+    idx = 1:stride:nSamples;
+    timeIntervals = ts.t(idx);
+
+    avgQLengths = cell(nStateful, jobCount);
+    for isf = 1:nStateful
+        for c = 1:jobCount
+            avgQLengths{isf, c} = ts.state{isf}(idx, c);
+        end
+    end
+
+    queueLengthMatrix = avgQLengths;
+end
